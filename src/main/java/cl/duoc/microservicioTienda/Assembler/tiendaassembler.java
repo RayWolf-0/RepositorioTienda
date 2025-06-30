b409fdcad5c1975e1894be6f9db38ceebf4e7b87
Lyla Winwolf <topalterluz.00.b@gmail.com>
2025-06-29 21:32:43 -0400

    documentacion faltante

---
+++ src/main/java/cl/duoc/microservicioTienda/Assembler/tiendaassembler.java
@@ -0,0 +1,5 @@
+package cl.duoc.microservicioTienda.Assembler;
+
+public class tiendaassembler {
+
+}
--- src/main/java/cl/duoc/microservicioTienda/model/Tienda.java
+++ src/main/java/cl/duoc/microservicioTienda/model/Tienda.java
@@ -1,5 +1,6 @@
 package cl.duoc.microservicioTienda.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,23 +16,31 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Entidad que representa una tienda")
+
 public class Tienda {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID_TIENDA")
+    @Schema(description = "id de la tienda")
+
     private int id_tienda;
 
     @Column(name= "NOMBRE", nullable = false, length = 100)
+    @Schema(description = "nombre de la tienda")
     private String nombre;
 
     @Column(name= "CIUDAD", nullable = false, length = 50)
+    @Schema(description = "ciudad de la tienda")
     private String ciudad;
 
     @Column(name= "DIRECCION",  length = 200)
+    @Schema(description = "direccion de la tienda")
     private String direccion; 
 
     @Column(name= "TELEFONO", nullable = false, length = 20)
+    @Schema(description = "telefono")
     private String telefono; 
 
 
